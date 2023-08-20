b6a3919812eda26fa445ba1a06e76a6a57763346
SHAHEEM593 <mohammadshaheem593@gmail.com>
2023-08-20 20:47:46 +0530

    first

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-@4*g6z_3iq4&vuqwp&$o4aeiuk108ztq!02cs!8fgyo3$)77h0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
